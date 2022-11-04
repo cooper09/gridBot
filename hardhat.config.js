fc0412e6f17e60aff6c5f80c7f50e0a24ff73174
cooper09 <cooperatkmba@gmail.com>
2022-11-04 10:02:44 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -28,17 +28,17 @@ module.exports = {
 
     hardhatfork: {
         chainId: 31337,
-        //url: "https://mainnet.infura.io/v3/4cd98623d90d401ca984c02080c6bf72",
+        //url: "https://mainnet.infura.io/v3/",
         url: "localhost:8545",
       },
     mainnet: {
         chainId: 1,
-        url: "https://mainnet.infura.io/v3/4cd98623d90d401ca984c02080c6bf72", // or any other JSON-RPC provider
-        accounts: ["0xa3994cf9f5df831441f98cbe07d1dbd4f2b1ac80518a1de7d66e6f7ef2a03a4a"]
+        url: "https://mainnet.infura.io/v3/2", // or any other JSON-RPC provider
+        accounts: [""]
       },
       matic: {
-        url: 'https://polygon-mainnet.g.alchemy.com/v2/-xVyH7D3cYUoEWpjV1xuaHXQcXL7va-X',
-        accounts: ['9b30f5dfe92f581ea1ad4d5d10bb49a746be4abaf39ec163a893414f698a0835'],
+        url: 'https://polygon-mainnet.g.alchemy.com/v2/',
+        accounts: [''],
       }
     }
 };
